0cb35b86d4a911c38b6c31c51886c3f0f87dad74
Milou Mohamed MilouMohamed <49353852+MilouMohamed@users.noreply.github.com>
2024-12-18 20:51:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "this Is Test Ecole hassan";
+echo "<br>";
+echo "this Is Test Ecole hassan";
+echo "<br>";
+echo "this Is Test Ecole hassan";
+echo "<br>";
+echo "this Is Test Ecole hassan";
+echo "<br>";
+echo "this Is Test Ecole hassan";
+echo "<br>";
+
+
+
+
+
+
+
+
+?>
